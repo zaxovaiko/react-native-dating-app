f9fb162d5d4d67e62acd3c0f2031044ecadd5e00
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2020-10-13 18:21:12 +0200

    Initiail commit.

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: () => React$Node = () => {
+	return <></>;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dating Application
+
+Dating Application based on React Native. We will use MERN stack for this project.
